a654ee8208785e4302a0d6336e9958d6490295ba
Andrew Barbour <andy.barbour@gmail.com>
2016-11-30 11:15:45 -0800

    Updated documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 # ##
-# Anything needed for the functioning of the namespace should 
-# be handled at load/unload times by the .onLoad and .onUnload 
-# hooks. For example, DLLs can be loaded (unless done by a 
-#                                         useDynLib directive in 
-#                                         the NAMESPACE file) 
-# and initialized in .onLoad and unloaded in .onUnload. 
-# Use .onAttach only for actions that are needed only when the 
-# package becomes visible to the user (for example a start-up message) 
+# Anything needed for the functioning of the namespace should
+# be handled at load/unload times by the .onLoad and .onUnload
+# hooks. For example, DLLs can be loaded (unless done by a
+#                                         useDynLib directive in
+#                                         the NAMESPACE file)
+# and initialized in .onLoad and unloaded in .onUnload.
+# Use .onAttach only for actions that are needed only when the
+# package becomes visible to the user (for example a start-up message)
 # or need to be run after the package environment has been created.
 
 #.onUnload <- function(libpath){
@@ -19,7 +19,7 @@
   pack <- 'stress'
   ##
   packageStartupMessage(
-    sprintf("Loaded %s (%s) -- stress inversion tools", pack, utils::packageVersion(pack))
+    sprintf("Loaded %s (%s) -- wroking with stresses and stress-inversion tools", pack, utils::packageVersion(pack))
     )
   ## options which can be reset by the user
   options(deform.ops=list())
